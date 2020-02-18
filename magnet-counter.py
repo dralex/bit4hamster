ee17e70380403f0e27fed804bab6877b9149ba35
Alexey Fedoseev <aleksey@fedoseev.net>
2020-02-18 22:23:28 +0300

    better control & led notification

--- magnet-counter.py
+++ magnet-counter.py
@@ -9,19 +9,33 @@
 # -----------------------------------------------------------------------------
 
 import microbit as m
+import math
 
 # Choose the appropriate value based on the cage & wheel configuration
 THRESHOLD = 13000
-
+  
 baseline = m.compass.get_field_strength() # Take a baseline reading of magnetic strength
 num = 0
 crossing = False
 
 while True:
     field = m.compass.get_field_strength()
+
     if not crossing and abs(field - baseline) > THRESHOLD:
-        crossing = True    
-        num = num + 1
-        m.display.show(num, wait = False, loop = True)
-    elif crossing and abs(field - baseline) <= THRESHOLD:
+	    crossing = True    
+            num = num + 1
+    elif crossing:
+        if abs(field - baseline) <= THRESHOLD:
+            crossing = False
+            m.display.clear()
+        else:
+            m.display.set_pixel(2, 2, 9)
+
+    if m.button_a.was_pressed():
+	m.display.show(num, wait = False, loop = True)
+    elif m.button_b.was_pressed():
+	baseline = m.compass.get_field_strength() # Take a baseline reading of magnetic strength
+        num = 0
         crossing = False
+        m.display.clear()
+
