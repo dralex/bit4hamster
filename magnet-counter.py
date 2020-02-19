632463982740a04652c8c56ee154184786ff9524
Alexey Fedoseev <aleksey@fedoseev.net>
2020-02-19 21:00:26 +0300

    add power & counter indication

--- magnet-counter.py
+++ magnet-counter.py
@@ -9,26 +9,35 @@
 # -----------------------------------------------------------------------------
 
 import microbit as m
+import math
 
 # Choose the appropriate value based on the cage & wheel configuration
 THRESHOLD = 13000
-  
+ 
+def update_level(nu):
+    n = int(nu / 200) + 1
+    for i in range(5):
+        if i < n:
+            m.display.set_pixel(i, 0, 9)
+        else:
+            m.display.set_pixel(i, 0, 0)
+
 baseline = m.compass.get_field_strength() # Take a baseline reading of magnetic strength
 num = 0
 crossing = False
 
+update_level(num)
 while True:
     field = m.compass.get_field_strength()
 
     if not crossing and abs(field - baseline) > THRESHOLD:
-	    crossing = True    
-            num = num + 1
-    elif crossing:
-        if abs(field - baseline) <= THRESHOLD:
-            crossing = False
-            m.display.clear()
-        else:
-            m.display.set_pixel(2, 2, 9)
+        crossing = True    
+        num = num + 1
+        m.display.set_pixel(2, 2, 9)
+        update_level(num)   
+    elif crossing and abs(field - baseline) <= THRESHOLD:
+        crossing = False
+        m.display.set_pixel(2, 2, 0)
 
     if m.button_a.was_pressed():
 	m.display.show(num, wait = False, loop = True)
@@ -37,4 +46,5 @@ while True:
         num = 0
         crossing = False
         m.display.clear()
+        update_level(num)
 
