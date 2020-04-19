ecf9e11a52fe76d976460b0a0bdb2363d97fbf33
Alexey Fedoseev <aleksey@fedoseev.net>
2020-04-19 09:44:22 +0300

    measure temp & light interval

--- magnet-histogram.py
+++ magnet-histogram.py
@@ -24,6 +24,7 @@ HIST_DELTA = 0.01
 HIST_ROUND_SIGNS = 1
 baseline = num = crossing = show_num = last_change = None
 num_buf = hist_dict = last_sync = file_num = None
+temp_sum = temp_count = light_sum = light_count = None
 
 def update_display():
     if show_num:
@@ -32,8 +33,27 @@ def update_display():
         m.display.set_pixel(0, 4, LIGHT)
         m.display.set_pixel(4, 4, LIGHT if crossing else 0)
 
+def get_temperature():
+    global temp_sum, temp_count  # pylint: disable=global-statement
+    temp_val = float(temp_sum) / temp_count
+    temp_sum = temp_count = 0
+    return temp_val
+
+def update_temperature():
+    global temp_sum, temp_count  # pylint: disable=global-statement
+    temp_sum += m.temperature()
+    temp_count += 1
+
 def get_light():
-    return m.pin0.read_analog()
+    global light_sum, light_count # pylint: disable=global-statement
+    light_val = float(light_sum) / light_count
+    light_sum = light_count = 0
+    return light_val
+
+def update_light():
+    global light_sum, light_count # pylint: disable=global-statement
+    light_sum += m.pin0.read_analog()
+    light_count += 1
 
 def calculate_files():
     global file_num # pylint: disable=global-statement
@@ -47,10 +67,12 @@ def remove_files():
 
 def reset():
     global baseline, num, crossing, show_num, last_change, num_buf, hist_dict, last_sync # pylint: disable=global-statement
+    global temp_sum, temp_count, light_sum, light_count # pylint: disable=global-statement
     baseline = m.compass.get_field_strength() # Take a baseline reading of magnetic strength
     num = 0
     crossing = False
     show_num = False
+    temp_sum = temp_count = light_sum = light_count = 0
     num_buf = []
     hist_dict = [0] * HIST_STEPS
     last_change = last_sync = time.ticks_ms() # pylint: disable=no-member
@@ -67,7 +89,7 @@ def save_hist_value(d):
 
 def sync_data():
     global last_sync # pylint: disable=global-statement
-    num_buf.append((last_sync, num, m.temperature(), get_light()))
+    num_buf.append((last_sync, num, get_temperature(), get_light()))
     f = open(LOG_FILENAME.format(file_num), 'w')
     for n in num_buf:
         f.write("{}\n".format(' '.join(map(str, n))))
@@ -88,6 +110,8 @@ while True:
         delta = t - last_change
         if delta > TIME_LIMIT:
             num = num + 1
+            update_temperature()
+            update_light()
             save_hist_value(delta)
             last_change = t
         update_display()
